7ac2737641a4834594b6d92212170170225ee2c1
Adeithe <28549467+Adeithe@users.noreply.github.com>
2024-03-05 08:34:01 -0500

    Refactor Twitch Ads API

--- api/twitch_ads.go
+++ api/twitch_ads.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"time"
 )
 
 type Commercial struct {
@@ -27,7 +28,7 @@ func NewAdsResource(client *Client) *AdsResource {
 }
 
 type AdsInsertRequest struct {
-	resource      *AdsResource
+	client        *Client
 	broadcasterID string
 	duration      int
 }
@@ -41,7 +42,7 @@ type AdsInsertResponse struct {
 //
 // Required Scope: channel:edit:commercial
 func (r *AdsResource) Insert(broadcasterId string) *AdsInsertRequest {
-	return &AdsInsertRequest{r, broadcasterId, 60}
+	return &AdsInsertRequest{r.client, broadcasterId, 60}
 }
 
 // Duration sets the duration of the commercial in seconds.
@@ -65,7 +66,7 @@ func (c *AdsInsertRequest) Do(ctx context.Context, opts ...RequestOption) (*AdsI
 		return nil, err
 	}
 
-	res, err := c.resource.client.doRequest(ctx, http.MethodPost, "/channels/commercial", bytes.NewReader(bs), opts...)
+	res, err := c.client.doRequest(ctx, http.MethodPost, "/channels/commercial", bytes.NewReader(bs), opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -77,6 +78,123 @@ func (c *AdsInsertRequest) Do(ctx context.Context, opts ...RequestOption) (*AdsI
 	}
 
 	return &AdsInsertResponse{
+		Header: res.Header,
+		Data:   data.Data,
+	}, nil
+}
+
+type AdsScheduleResource struct {
+	client *Client
+}
+
+func NewAdsScheduleResource(client *Client) *AdsScheduleResource {
+	return &AdsScheduleResource{client}
+}
+
+type AdsScheduleListRequest struct {
+	client *Client
+	opts   []RequestOption
+}
+
+type AdsScheduleListResponse struct {
+	Header http.Header
+	Data   []AdSchedule
+}
+
+type AdSchedule struct {
+	Duration        int       `json:"duration"`
+	NextAdAt        time.Time `json:"next_ad_at"`
+	LastAdAt        time.Time `json:"last_ad_at"`
+	PrerollFreeTime int       `json:"preroll_free_time"`
+	SnoozeCount     int       `json:"snooze_count"`
+	SnoozeRefreshAt time.Time `json:"snooze_refresh_at"`
+}
+
+// List returns ad schedule related information, including snooze, when the last ad was run,
+// when the next ad is scheduled, and if the channel is currently in pre-roll free time.
+//
+// NOTE: A new advertisement can NOT be run until 8 minutes running the previous.
+//
+// Required Scope: channel:read:ads
+func (r *AdsScheduleResource) List(broadcasterId string) *AdsScheduleListRequest {
+	return &AdsScheduleListRequest{
+		client: r.client,
+		opts: []RequestOption{
+			AddQueryParameter("broadcaster_id", broadcasterId),
+		},
+	}
+}
+
+// Do executes the request.
+func (r *AdsScheduleListRequest) Do(ctx context.Context, opts ...RequestOption) (*AdsScheduleListResponse, error) {
+	res, err := r.client.doRequest(ctx, http.MethodGet, "/channels/ads", nil, append(r.opts, opts...)...)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	data, err := decodeResponse[AdSchedule](res)
+	if err != nil {
+		return nil, err
+	}
+
+	return &AdsScheduleListResponse{
+		Header: res.Header,
+		Data:   data.Data,
+	}, nil
+}
+
+type AdsSnoozeResource struct {
+	client *Client
+}
+
+func NewAdsSnoozeResource(client *Client) *AdsSnoozeResource {
+	return &AdsSnoozeResource{client}
+}
+
+type AdsSnoozeRequest struct {
+	client *Client
+	opts   []RequestOption
+}
+
+type AdsSnoozeResponse struct {
+	Header http.Header
+	Data   []AdSnooze
+}
+
+type AdSnooze struct {
+	SnoozeCount     int       `json:"snooze_count"`
+	SnoozeRefreshAt time.Time `json:"snooze_refresh_at"`
+	NextAdAt        time.Time `json:"next_ad_at"`
+}
+
+// Insert if available, pushes back the timestamp of the upcoming automatic advertisement mid-roll by 5 minutes.
+// This endpoint duplicates the snooze functionality in the creator dashboard's Ad Manager.
+//
+// Required Scope: channel:manage:ads
+func (r *AdsSnoozeResource) Insert(broadcasterId string) *AdsSnoozeRequest {
+	return &AdsSnoozeRequest{
+		client: r.client,
+		opts: []RequestOption{
+			AddQueryParameter("broadcaster_id", broadcasterId),
+		},
+	}
+}
+
+// Do executes the request.
+func (r *AdsSnoozeRequest) Do(ctx context.Context, opts ...RequestOption) (*AdsSnoozeResponse, error) {
+	res, err := r.client.doRequest(ctx, http.MethodPost, "/channels/ads/schedule/snooze", nil, append(r.opts, opts...)...)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	data, err := decodeResponse[AdSnooze](res)
+	if err != nil {
+		return nil, err
+	}
+
+	return &AdsSnoozeResponse{
 		Header: res.Header,
 		Data:   data.Data,
 	}, nil
